9ad2dddd66ab6ae66eed016e6cd3190ebfaada90
Jannik Huschka <huschka@neuland.dev>
2022-11-02 11:02:02 +0100

    Added name method for authors

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Author, type: :model do
-    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
-    expect(author.name).to eq('Alan Turing')
+    it "should have a first name, last name, and homepage" do
+        author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+        expect(author.name).to eq('Alan Turing')
+    end
 end
