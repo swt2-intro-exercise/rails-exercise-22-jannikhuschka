e9b301c692b6e080c2436a233b3263cc0a07d092
Jannik Huschka <huschka@neuland.dev>
2022-11-02 20:09:46 +0100

    Added show buttons for each author

--- spec/features/author/show_authors_index_spec.rb
+++ spec/features/author/show_authors_index_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe "Show authors index page", type: :feature do
-    it "should exist at 'author_path', render without error and list all authors" do
+    it "should exist at 'authors_path', render without error and list all authors" do
         author1 = FactoryBot.create :turing
         author2 = FactoryBot.create :einstein
         visit authors_path
         expect(page).to have_text("Author index")
         expect(page).to have_text(author1.name)
         expect(page).to have_text(author2.name)
+        expect(page).to have_link 'Show', href: author_path(author1)
+        expect(page).to have_link 'Show', href: author_path(author2)
         expect(page).to have_link 'New', href: new_author_path
     end
 end
