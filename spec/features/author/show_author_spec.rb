9eef07f98ee3209361c7f82ed51f8a88b829e596
Jannik Huschka <huschka@neuland.dev>
2022-11-02 14:31:21 +0100

    Show page shows author's details correctly

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -4,7 +4,13 @@ describe "Show author page", type: :feature do
     it "should exist at 'author_path' and render withour error" do
         alan = FactoryBot.create :author
         visit author_path(alan)
-        expect(page).to have_text(alan.name)
-        expect(page).to have_link 'Hommage' , href: alan.homepage
     end
+
+    it "should exist at 'author_path' and render withour error" do
+        alan = FactoryBot.create :author
+        visit author_path(alan)
+        expect(page).to have_text(alan.name)
+        expect(page).to have_link 'Homepage' , href: alan.homepage
+    end
+
 end
