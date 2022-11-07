1b656111ec945b005f008a3f9574e715a31a47dc
Jannik Huschka <huschka@neuland.dev>
2022-11-07 15:07:14 +0100

    Added authors to paper view

--- spec/features/paper/show.html.erb_spec.rb
+++ spec/features/paper/show.html.erb_spec.rb
@@ -21,7 +21,7 @@ describe "Show paper page", type: :feature do
     it "should display author(s) for a paper" do
         alan = FactoryBot.create :turing
         paper = FactoryBot.create :paper
-        # paper.authors << alan
+        paper.authors << alan
         visit paper_path(paper)
         expect(page).to have_text("Alan Turing")
     end
