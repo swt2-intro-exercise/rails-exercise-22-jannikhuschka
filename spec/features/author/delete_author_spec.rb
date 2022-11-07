1a84db28e3a6e6e044b0bbe09fd5fbff30678b16
Jannik Huschka <huschka@neuland.dev>
2022-11-07 11:49:51 +0100

    Added delete functionality

--- spec/features/author/delete_author_spec.rb
+++ spec/features/author/delete_author_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 describe "Author index page", type: :feature do
     it "should have a link to delete an author" do
+        alan = FactoryBot.create :turing
         visit authors_path
-        expect(page).to have_link('Delete', href: author_path(@alan_turing))
+        expect(page).to have_button 'Delete'
+    end
+
+    it "should delete an author" do
+        alan = FactoryBot.create :turing
+        visit authors_path
+        click_button('Delete')
+        expect(Author.find_by(id: alan.id)).to be_nil
     end
 end
