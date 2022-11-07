5d410a411af4e3359e519797190076460049fe9e
Jannik Huschka <huschka@neuland.dev>
2022-11-07 11:57:07 +0100

    Adapted test

--- spec/features/author/delete_author_spec.rb
+++ spec/features/author/delete_author_spec.rb
@@ -4,13 +4,13 @@ describe "Author index page", type: :feature do
     it "should have a link to delete an author" do
         alan = FactoryBot.create :turing
         visit authors_path
-        expect(page).to have_button 'Delete'
+        expect(page).to have_link('Delete', href: author_path(alan))
     end
 
     it "should delete an author" do
         alan = FactoryBot.create :turing
         visit authors_path
-        click_button('Delete')
+        click_link('Delete')
         expect(Author.find_by(id: alan.id)).to be_nil
     end
 end
